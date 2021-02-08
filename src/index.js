fa584b177b468a8f7a4252c656685114701ddf5e
vladdominator <73780858+vladdominator@users.noreply.github.com>
2021-02-08 03:46:11 +0300

    feat: for hundred-thouthand

--- src/index.js
+++ src/index.js
@@ -42,5 +42,56 @@ module.exports = function toReadable (number) {
          return strDecimals;
       }
    }
-
+   if(number >= 100 && number < 1000){
+      let strHundreds = 'hundred';
+      let hundreds = Math.floor(number / 100);
+      let values = number % 100;
+      for(let i = 0; i < numberCount.length; i++){
+         if(hundreds === i && Number.isInteger(number / 100)){
+            return numberCount[i] + " " + strHundreds;
+         }
+         else if(hundreds === i){
+            if(values > 10 && values < 20){
+               for(let j = 0; j < numberTwo.length; j++){
+                  if(values % 10 === j + 1){
+                     return numberCount[i] + " " + strHundreds + " " + numberTwo[j];
+                  }
+               }
+            }
+            else if(values > 0 && values <= 10){
+               for(let j = 0; j < numberCount.length; j++){
+                  if(values === j){
+                     return numberCount[i] + " " + strHundreds + " " + numberCount[j];
+                  }
+               }
+            }
+            else if(values >= 20 && values < 100){
+               let strElements;
+               let strDecimals;
+               let element = values % 10;
+               if(Number.isInteger(values / 10)){
+                  strElements = '';
+               }
+               else{
+                  for(let j = 0; j < numberCount.length; j++){
+                     if(element === j){
+                        strElements = numberCount[j];
+                     }
+                  }
+               }
+               for(let j = 0; j < numberTens.length; j++){
+                  if(Math.floor(values / 10) === j + 2){
+                     strDecimals = numberTens[j];
+                  }
+               }
+               if(strElements){
+                  return numberCount[i] + " " + strHundreds + " " + strDecimals + " " + strElements;
+               }
+               else{
+                  return numberCount[i] + " " + strHundreds + " " + strDecimals;
+               }
+            }
+         }
+      }
+   }
 }
