c8057bcc3c1e99bcb3d868be0da4ad4d74a10601
vladdominator <73780858+vladdominator@users.noreply.github.com>
2021-02-08 03:27:41 +0300

    feat: for one-ten

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+   let numberCount = ['zero','one','two','three','four','five','six','seven','eight','nine','ten'];
+   let numberTwo = ['eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+   let numberTens = ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+   if(number >= 0 && number <= 10){
+      for(let i = 0; i < numberCount.length; i++){
+         if(number === i){
+            return numberCount[i];
+         }
+      }
+   }
 }
